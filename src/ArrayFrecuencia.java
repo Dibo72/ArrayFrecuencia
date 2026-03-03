1f257bdc681f4e36e2095997750f8f51545e4406
Dibo72 <humanida133@gmail.com>
2026-03-03 13:18:46 +0100

    Ejercicio acabado

--- src/ArrayFrecuencia.java
+++ src/ArrayFrecuencia.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class ArrayFrecuencia {
+    public static void main(String[] args) {
+        int[] repetidos = new int[10];
+        int[] frecuencia = new int[10];
+
+        //introducimos valores random al array
+        for (int i = 0; i < repetidos.length; i++) {
+            repetidos[i] =(int) (Math.random()*9);
+        }
+
+        //lo mostramos
+        System.out.println("Array:");
+
+        for (int i = 0; i < repetidos.length; i++) {
+            System.out.println((repetidos[i]));
+        }
+
+        System.out.println();
+
+        //si el valor del array es igual a la posicion que estamos mirando, el contador sube
+        for (int i = 0; i < frecuencia.length; i++) {
+            int contador = 0;
+            for (int j = 0; j < repetidos.length; j++) {
+                if (repetidos[j] == i) {
+                    contador++;
+                }
+            }
+            //cuando ya hemos comprobado todos lo añadimos a freciuencia
+            frecuencia[i] = contador;
+        }
+
+        //mostramos el array de la frecuencia
+        System.out.println("Frecuencia:");
+        for (int i = 0; i < frecuencia.length; i++) {
+            System.out.println(frecuencia[i]);
         }
     }
 }
